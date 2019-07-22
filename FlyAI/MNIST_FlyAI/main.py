7e734ea2889884d0012a8b641b6200902f5cd7cd
ArmstrongLLQ <armstrong_lab@163.com>
2019-07-22 18:52:58 +0800

    sync

--- FlyAI/MNIST_FlyAI/main.py
+++ FlyAI/MNIST_FlyAI/main.py
@@ -11,12 +11,17 @@ import argparse
 from flyai.dataset import Dataset
 from keras.layers import Dense, Dropout, Conv2D, MaxPool2D, Flatten
 from keras.models import Sequential
+import keras
 
 from model import Model
 from path import MODEL_PATH
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-e", "--EPOCHS", default=10, type=int, help="train epochs")
+parser.add_argument("-e",
+                    "--EPOCHS",
+                    default=10,
+                    type=int,
+                    help="train epochs")
 parser.add_argument("-b", "--BATCH", default=16, type=int, help="batch size")
 args = parser.parse_args()
 '''
@@ -30,32 +35,43 @@ dataset = Dataset(epochs=args.EPOCHS, batch=args.BATCH)
 model = Model(dataset)
 
 sqeue = Sequential()
-# 第一个卷积层，32个卷积核，大小５x5，卷积模式SAME,激活函数relu,输入张量的大小
-sqeue.add(Conv2D(filters=32, kernel_size=(5, 5), padding='Same', activation='relu',
-                 input_shape=(28, 28, 1)))
-sqeue.add(Conv2D(filters=32, kernel_size=(5, 5), padding='Same', activation='relu'))
-# 池化层,池化核大小２x2
-sqeue.add(MaxPool2D(pool_size=(2, 2)))
-# 随机丢弃四分之一的网络连接，防止过拟合
-sqeue.add(Dropout(0.25))
-sqeue.add(Conv2D(filters=64, kernel_size=(3, 3), padding='Same', activation='relu'))
-sqeue.add(Conv2D(filters=64, kernel_size=(3, 3), padding='Same', activation='relu'))
-sqeue.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
-sqeue.add(Dropout(0.25))
-# 全连接层,展开操作，
-sqeue.add(Flatten())
-# 添加隐藏层神经元的数量和激活函数
-sqeue.add(Dense(256, activation='relu'))
-sqeue.add(Dropout(0.25))
-# 输出层
-sqeue.add(Dense(10, activation='softmax'))
+
+# # 第一个卷积层，32个卷积核，大小５x5，卷积模式SAME,激活函数relu,输入张量的大小
+# sqeue.add(Conv2D(filters=32, kernel_size=(5, 5), padding='Same', activation='relu',
+#                  input_shape=(28, 28, 1)))
+# sqeue.add(Conv2D(filters=32, kernel_size=(5, 5), padding='Same', activation='relu'))
+# # 池化层,池化核大小２x2
+# sqeue.add(MaxPool2D(pool_size=(2, 2)))
+# # 随机丢弃四分之一的网络连接，防止过拟合
+# sqeue.add(Dropout(0.25))
+# sqeue.add(Conv2D(filters=64, kernel_size=(3, 3), padding='Same', activation='relu'))
+# sqeue.add(Conv2D(filters=64, kernel_size=(3, 3), padding='Same', activation='relu'))
+# sqeue.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+# sqeue.add(Dropout(0.25))
+# # 全连接层,展开操作，
+# sqeue.add(Flatten())
+# # 添加隐藏层神经元的数量和激活函数
+# sqeue.add(Dense(256, activation='relu'))
+# sqeue.add(Dropout(0.25))
+# # 输出层
+# sqeue.add(Dense(10, activation='softmax'))
 
 # 输出模型的整体信息
 # 总共参数数量为784*512+512 + 512*512+512 + 512*10+10 = 669706
+
+sqeue.add(
+    Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)))
+sqeue.add(Conv2D(64, (3, 3), activation='relu'))
+sqeue.add(MaxPool2D(pool_size=(2, 2)))
+sqeue.add(Dropout(0.25))
+sqeue.add(Flatten())
+sqeue.add(Dense(128, activation='relu'))
+sqeue.add(Dropout(0.5))
+sqeue.add(Dense(10, activation='softmax'))
 sqeue.summary()
 
 sqeue.compile(loss='categorical_crossentropy',
-              optimizer='adam',
+              optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
 
 best_score = 0
@@ -63,7 +79,8 @@ for step in range(dataset.get_step()):
     first_time = int(time.time())
     x_train, y_train = dataset.next_train_batch()
     x_val, y_val = dataset.next_validation_batch()
-    history = sqeue.fit(x_train, y_train,
+    history = sqeue.fit(x_train,
+                        y_train,
                         batch_size=args.BATCH,
                         verbose=1,
                         validation_data=(x_val, y_val))
--- keras-study/keras-master/examples/imdb_bidirectional_lstm.py
+++ keras-study/keras-master/examples/imdb_bidirectional_lstm.py
@@ -24,7 +24,7 @@ print('Loading data...')
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
 print(len(x_train), 'train sequences')
 print(len(x_test), 'test sequences')
-
+ 
 print('Pad sequences (samples x time)')
 x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
 x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
